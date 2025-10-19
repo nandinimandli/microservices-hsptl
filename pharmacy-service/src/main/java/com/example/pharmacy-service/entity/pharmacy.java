047c8ce0ebe7828582f97717626e18b8231e5281
nandinimandli <mandlinandini.1124@gmail.com>
2025-10-20 02:42:49 +0530

    first commit

--- pharmacy-service/src/main/java/com/example/pharmacy-service/entity/pharmacy.java
+++ pharmacy-service/src/main/java/com/example/pharmacy-service/entity/pharmacy.java
@@ -1,9 +1,3 @@
-package com.example.pharmacyservice.entity;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.*;
-import java.util.List;
-
 @Entity
 public class Pharmacy {
 
@@ -11,7 +5,7 @@ public class Pharmacy {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String warehouseName;
+    private String pharmacyName; // renamed from warehouseName
 
     @OneToMany(mappedBy = "pharmacy", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
@@ -26,12 +20,12 @@ public class Pharmacy {
         this.id = id;
     }
 
-    public String getWarehouseName() {
-        return warehouseName;
+    public String getPharmacyName() {
+        return pharmacyName;
     }
 
-    public void setWarehouseName(String warehouseName) {
-        this.warehouseName = warehouseName;
+    public void setPharmacyName(String pharmacyName) {
+        this.pharmacyName = pharmacyName;
     }
 
     public List<PharmacyItem> getItems() {
